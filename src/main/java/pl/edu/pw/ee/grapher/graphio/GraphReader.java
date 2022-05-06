2a115269eca44353c73beaab422205f19692defb
Szymon Poltorak <szymonpotorak@gmail.com>
2022-05-06 20:59:12 +0200

    Dodano GraphReader'a oraz przygotowano template pod testy

--- src/main/java/pl/edu/pw/ee/grapher/graphio/GraphReader.java
+++ src/main/java/pl/edu/pw/ee/grapher/graphio/GraphReader.java
@@ -1,11 +1,102 @@
 package pl.edu.pw.ee.grapher.graphio;
 
+import org.jetbrains.annotations.NotNull;
 import pl.edu.pw.ee.grapher.graph.Graph;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static pl.edu.pw.ee.grapher.Constants.*;
 
 public class GraphReader{
-    public static void readFromFile(Graph graph, File graphFile){
+    public static @NotNull Graph readFromFile(File graphFile) throws FileNotFoundException {
+        Scanner in = new Scanner(graphFile);
+        int rows = in.nextInt();
+        int columns = in.nextInt();
 
+        if (rows <= 0 || columns <= 0){
+            in.close();
+            throw new IllegalArgumentException("No columns or rows found!");
+        }
+        Graph graph = new Graph(rows, columns);
+
+        for (int i = 0; i < graph.getNumOfVertices(); i++){
+            String line = in.nextLine();
+
+            if (line == null){
+                in.close();
+                throw new NullPointerException("There is nothing i can read!!");
+            }
+
+            if (!insertGraph(graph, i, line)){
+                in.close();
+                throw new IllegalStateException("I was not able to insert graph!");
+            }
+        }
+
+        in.close();
+        return graph;
+    }
+
+    public static boolean insertGraph(@NotNull Graph graph, int index, String line){
+        Scanner in = new Scanner(line);
+        int rows = graph.getRows();
+        int columns = graph.getColumns();
+
+        while (in.hasNext()){
+            boolean hasBeenAdded = false;
+            int vertex = in.nextInt();
+            float weight = in.nextFloat();
+
+            if (index - columns >= 0 && index - columns < columns * rows){
+                if (vertex == index - columns){
+                    graph.getVertex(index).setExistence(UP, true);
+                    graph.getVertex(index).setConnections(UP, index - columns);
+                    graph.getVertex(index).setWeight(UP, weight);
+                    hasBeenAdded = true;
+                }
+            } else {
+                graph.getVertex(index).setExistence(UP, false);
+            }
+            if(index + 1 < columns * rows && (index+1)%columns != 0){
+                if (vertex == index + 1){
+                    graph.getVertex(index).setExistence(RIGHT, true);
+                    graph.getVertex(index).setConnections(RIGHT, index + 1);
+                    graph.getVertex(index).setWeight(RIGHT, weight);
+                    hasBeenAdded = true;
+                }
+            } else {
+                graph.getVertex(index).setExistence(RIGHT, false);
+            }
+            if (index + columns > 0 && index + columns < columns * rows){
+                if(vertex == index + columns){
+                    graph.getVertex(index).setExistence(DOWN, true);
+                    graph.getVertex(index).setConnections(DOWN, index + columns);
+                    graph.getVertex(index).setWeight(DOWN, weight);
+                    hasBeenAdded = true;
+                }
+            } else {
+                graph.getVertex(index).setExistence(DOWN, false);
+            }
+            if(index - 1 >= 0 && index%columns != 0){
+                if(vertex == index - 1){
+                    graph.getVertex(index).setExistence(LEFT, true);
+                    graph.getVertex(index).setConnections(LEFT, index - 1);
+                    graph.getVertex(index).setWeight(LEFT, weight);
+                    hasBeenAdded = true;
+                }
+            } else {
+                graph.getVertex(index).setExistence(LEFT, false);
+            }
+
+            if (!hasBeenAdded){
+                in.close();
+                return false;
+            }
+        }
+
+        in.close();
+        return true;
     }
 }
--- src/test/java/pl/edu/pw/ee/grapher/graphio/GraphReaderTest.java
+++ src/test/java/pl/edu/pw/ee/grapher/graphio/GraphReaderTest.java
@@ -1,5 +1,35 @@
 package pl.edu.pw.ee.grapher.graphio;
 
-public class GraphReaderTest {
+import org.junit.jupiter.api.Test;
 
+public class GraphReaderTest {
+    @Test
+    public void readFromFile_test_pass(){
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void readFromFile_test_IllegalArgument_exception(){
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void readFromFile_test_FileNotFound_exception(){
+        //given
+
+        //when
+
+        //then
+
+    }
 }
