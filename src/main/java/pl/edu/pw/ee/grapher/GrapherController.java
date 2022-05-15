d2cc17e840ad0c3d3ae77ab87447d9f9e13d2d85
Sebastian Sikorski <sebastian.sikorski0702@gmail.com>
2022-05-15 19:44:25 +0200

    Implementacja interakcji z plikami w GUI

--- src/main/java/pl/edu/pw/ee/grapher/GrapherController.java
+++ src/main/java/pl/edu/pw/ee/grapher/GrapherController.java
@@ -3,12 +3,15 @@ package pl.edu.pw.ee.grapher;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import pl.edu.pw.ee.grapher.generator.EdgeMode;
+import javafx.stage.FileChooser;
 import pl.edu.pw.ee.grapher.generator.WageMode;
 import pl.edu.pw.ee.grapher.graph.Graph;
+import pl.edu.pw.ee.grapher.graphio.GraphReader;
+import pl.edu.pw.ee.grapher.graphio.GraphSaver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,23 +22,53 @@ public class GrapherController implements Initializable {
     private TextField columnInput, rowsInput, startInput, endInput, fileInput, pointsInput, consoleOutput;
 
     private EntryData userData;
-    private Graph graph;
+    private Graph graph = null;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         userData = new EntryData();
+        fileInput.setEditable(false);
 
         genButton.setOnMouseClicked(event -> {
             userData.setColumns(Integer.parseInt(columnInput.getText()));
             userData.setRows(Integer.parseInt(rowsInput.getText()));
             userData.setRangeEnd(Float.parseFloat(endInput.getText()));
             userData.setRangeStart(Float.parseFloat(startInput.getText()));
+            userData.setMode(1);
 
-            var gen = new WageMode();
-            var graph = new Graph(userData.getRows(),userData.getColumns());
-            gen.generate(graph,userData);
+            if(userData.getMode() == 1) {
+                var ggen = new WageMode();
+                graph = new Graph(userData.getRows(), userData.getColumns());
+                ggen.generate(graph, userData);
+            }
 
+            //System.out.println(userData);
+            //System.out.println(graph);
 
+        });
+
+        saveButton.setOnMouseClicked(event -> {
+            File file = null;
+            FileChooser fc = new FileChooser();
+            file = fc.showOpenDialog(null);
+
+            if (file != null) {
+                GraphSaver.saveToFile(graph, file);
+                fileInput.setText(file.getName());
+            }
+        });
+
+        openButton.setOnMouseClicked(event -> {
+            File file = null;
+            FileChooser fc = new FileChooser();
+            file = fc.showOpenDialog(null);
+
+            try {
+                graph = GraphReader.readFromFile(file);
+                fileInput.setText(file.getName());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         });
     }
 }
