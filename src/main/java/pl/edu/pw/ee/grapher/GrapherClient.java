04762e0b101e39d330b30b6b42dba82289b6f5fc
Szymon Poltorak <szymonpotorak@gmail.com>
2022-05-20 10:45:36 +0200

    Refaktoryzacja kodu css

--- src/main/java/pl/edu/pw/ee/grapher/GrapherClient.java
+++ src/main/java/pl/edu/pw/ee/grapher/GrapherClient.java
@@ -17,9 +17,11 @@ public class GrapherClient extends Application {
         var width = 1280;
         var height = 720;
         var scene = new Scene(fxmlLoader.load(), width, height);
-        var main = Objects.requireNonNull(this.getClass().getResource("css/grapher.css")).toExternalForm();
+        var texts = Objects.requireNonNull(this.getClass().getResource("css/texts.css")).toExternalForm();
+        var buttons = Objects.requireNonNull(this.getClass().getResource("css/buttons.css")).toExternalForm();
+        var panes = Objects.requireNonNull(this.getClass().getResource("css/panes.css")).toExternalForm();
 
-        scene.getStylesheets().add(main);
+        scene.getStylesheets().addAll(texts, buttons, panes);
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("img/icon.png")).toString()));
         stage.setTitle("Grapher");
         stage.setResizable(false);
