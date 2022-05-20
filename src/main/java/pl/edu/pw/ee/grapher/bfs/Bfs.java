a1f916771d711106ea244da8dd7f196d4a4bbf19
Sebastian Sikorski <sebastian.sikorski0702@gmail.com>
2022-05-20 13:59:24 +0200

    Rozpoczęcie prac

--- src/main/java/pl/edu/pw/ee/grapher/bfs/Bfs.java
+++ src/main/java/pl/edu/pw/ee/grapher/bfs/Bfs.java
@@ -2,58 +2,69 @@ package pl.edu.pw.ee.grapher.bfs;
 
 import org.jetbrains.annotations.NotNull;
 import pl.edu.pw.ee.grapher.graph.Graph;
+import pl.edu.pw.ee.grapher.graph.Vertex;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Bfs {
     private Bfs() {}
 
-    public static boolean checkIfCoherent(@NotNull Graph graph){
-        var queue = new int[graph.getNumOfVertices()];
-        Arrays.fill(queue, -1);
+    public static boolean checkIfCoherent(@NotNull Graph graph) {
+        boolean coherent = true;
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        boolean[] visited = new boolean[graph.getNumOfVertices()];
+        Arrays.fill(visited, false);
+        queue.add(0);
 
-        for (int i = 0; i < graph.getNumOfVertices(); i++){
-            var visited = new boolean[graph.getNumOfVertices()];
-            int reader = 0;
-            int writer = 0;
+        while (!queue.isEmpty()) {
+            int currentIndex = queue.getFirst();
+            Vertex currentVertex = graph.getVertex(currentIndex);
 
-            queue[writer] = i;
-            visited[i] = true;
-            writer = Bfs.calculateVertex(writer, graph.getNumOfVertices());
-
-            while (reader != writer){
-                int currentVertex = queue[reader];
-                reader = Bfs.calculateVertex(reader, graph.getNumOfVertices());
-
-                for (int j = 0; j < 4; j++){
-                    int aim = graph.getVertex(currentVertex).getConnection(j);
-
-                    if (graph.getVertex(currentVertex).getExistence(j) && !visited[aim]){
-                        queue[writer] = aim;
-                        visited[aim] = true;
-                        writer = Bfs.calculateVertex(writer, graph.getNumOfVertices());
+            for (int i = 0; i < 4; i++) {
+                if (currentVertex.getExistence(i)) {
+                    if (!visited[currentVertex.getConnection(i)]) {
+                        queue.add(currentVertex.getConnection(i));
+                        visited[currentVertex.getConnection(i)] = true;
                     }
                 }
             }
-
-            if (!Bfs.checkIfVisited(visited, graph.getNumOfVertices())){
-                return false;
-            }
+        }
+        for (boolean b : visited) {
+            if (!b)
+                coherent = false;
         }
 
-        return true;
-    }
+        Graph invertedGraph = invertGraph(graph);
 
-    private static boolean checkIfVisited(boolean[] visited, int numOfVertices){
-        for (int k = 0; k < numOfVertices; k++){
-            if (!visited[k]){
-                return false;
+        Arrays.fill(visited, false);
+        queue.clear();
+        queue.add(0);
+
+        while (!queue.isEmpty()) {
+            int currentIndex = queue.getFirst();
+            Vertex currentVertex = invertedGraph.getVertex(currentIndex);
+
+            for (int i = 0; i < 4; i++) {
+                if (currentVertex.getExistence(i)) {
+                    if (!visited[currentVertex.getConnection(i)]) {
+                        queue.add(currentVertex.getConnection(i));
+                        visited[currentVertex.getConnection(i)] = true;
+                    }
+                }
             }
         }
-        return true;
+        for (boolean b : visited) {
+            if (!b)
+                coherent = false;
+        }
+
+
+    return coherent;
     }
 
-    public static int calculateVertex(int index, int numOfVertices){
-        return (index + 1) % numOfVertices;
+    private static Graph invertGraph(Graph graph) {
+        return new Graph(3,3);
     }
+
 }
